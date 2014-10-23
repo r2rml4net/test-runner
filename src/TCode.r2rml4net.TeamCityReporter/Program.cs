f11287b3d06619e7f0811949afc79cd3a737023b
tpluscode <tpluscode@users.noreply.github.com>
2014-10-23 10:58:37 +0200

    improved test reported

---
+++ src/TCode.r2rml4net.TeamCityReporter/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TCode.r2rml4net.TeamCityReporter
+{
+    using System;
+
+    using CommandLine;
+
+    public class Program
+    {
+        private static int Main(string[] args)
+        {
+            var options = new Options();
+            var parseSuccess = Parser.Default.ParseArguments(args, options);
+
+            if (!parseSuccess)
+            {
+                Console.WriteLine("Invalid args");
+                return 1;
+            }
+
+            new Reporter(options).ProcessResults();
+
+            return 0;
+        }
+    }
+}
--- src/TCode.r2rml4net.TeamCityReporter/Reporter.cs
+++ src/TCode.r2rml4net.TeamCityReporter/Reporter.cs
@@ -1,6 +1,9 @@
 ﻿namespace TCode.r2rml4net.TeamCityReporter
 {
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
 
     using VDS.RDF;
     using VDS.RDF.Parsing;
@@ -9,12 +12,20 @@
 
     public class Reporter
     {
-        static void Main(string[] args)
+        private readonly Options _options;
+        private static readonly Regex TestRegex = new Regex(@"^http://www.w3.org/2001/sw/rdb2rdf/test-cases/#(?<type>\w+)TC(?<number>\d+?)(?<variant>[a-z]?)$");
+
+        public Reporter(Options options)
         {
-            Console.WriteLine("##teamcity[testSuiteStarted name='{0}']", args[0]);
+            this._options = options;
+        }
+
+        public void ProcessResults()
+        {
+            Console.WriteLine("##teamcity[testSuiteStarted name='{0}']", _options.Report);
 
             var store = new TripleStore();
-            store.LoadFromFile(args[0]);
+            store.LoadFromFile(_options.Report);
 
             var queryProcessor = new LeviathanQueryProcessor(store);
 
@@ -30,6 +41,8 @@
             {
                 Console.WriteLine("##teamcity[testStarted name='{0}']", result["test"]);
 
+                PrintTestOutput(result["test"].ToString());
+
                 if (((IUriNode)result["outcome"]).Uri.ToString() == "http://www.w3.org/ns/earl#fail")
                 {
                     Console.WriteLine("##teamcity[testFailed name='{0}']", result["test"]);
@@ -38,7 +51,40 @@
                 Console.WriteLine("##teamcity[testFinished name='{0}']", result["test"]);
             }
 
-            Console.WriteLine("##teamcity[testSuiteFinished name='{0}']", args[0]);
+            Console.WriteLine("##teamcity[testSuiteFinished name='{0}']", _options.Report);
+        }
+
+        private void PrintTestOutput(string test)
+        {
+            try
+            {
+                var matchCollection = TestRegex.Matches(test);
+
+                var testNum = int.Parse(matchCollection[0].Groups["number"].ToString());
+                var testType = matchCollection[0].Groups["type"].ToString();
+                var variant = matchCollection[0].Groups["variant"].ToString();
+
+                var directory = Directory.GetDirectories(_options.CasesPath, string.Format("D{0:000}*", testNum)).Single();
+
+                var fileBaseName = testType == "R2RML" ? "mapped" : "directGraph";
+                var outFile = string.Format("{0}{1}-r2rml4net.*", fileBaseName, variant);
+
+                var outFilePath = Directory.GetFiles(directory, outFile).SingleOrDefault();
+
+                if (outFilePath != null)
+                {
+                    Console.WriteLine(File.ReadAllText(outFilePath));
+                }
+                else
+                {
+                    Console.WriteLine("Output file not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Cannot open file");
+                Console.Error.WriteLine(ex.Message);
+            }
         }
     }
 }
