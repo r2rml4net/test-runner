d74dc512c531a9587c99008ade74a8a37fb4f9e2
tpluscode <tpluscode@users.noreply.github.com>
2020-07-31 10:57:24 +0200

    add params to run script

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-[[ -n $1 ]] && CASES="./test-cases/$1*" || CASES="./test-cases/*"
+grep=""
+NoReport=0
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -g|--grep) grep="$2"; shift ;;
+        --no-report) NoReport=1 ;;
+        -s) Suffix="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+CASES="./test-cases/$grep*"
 PASSWORD="Passw0rd1"
 ConnectionString="Server=tcp:localhost,1433;User ID=sa;Password=$PASSWORD"
 
@@ -24,7 +37,7 @@ do
   echo "Running direct mapping"
   r2rml4net direct \
     -c "$CASE_ConnectionString" \
-    -o "$caseDir/directGraph-r2rml4net.ttl" \
+    -o "$caseDir/directGraph-$Suffix.ttl" \
     -b http://example.com/base/ \
     --preserve-duplicate-rows
 
@@ -35,7 +48,7 @@ do
   shopt -s nullglob
   for rml in $RML_CASES
   do
-    RML_OUT=$(echo "$rml" | sed -e 's/r2rml/mapped/g' -e 's/\.ttl$/-r2rml4net.nq/g')
+    RML_OUT=$(echo "$rml" | sed -e 's/r2rml/mapped/g' -e "s/\.ttl$/-$Suffix.nq/g")
     r2rml4net rml \
       -c "$CASE_ConnectionString" \
       -m "$rml" \
@@ -44,10 +57,13 @@ do
   done
 done
 
-echo "Generating EARL reports"
-(
-  cd "test harness"
-  ./rdb2rdf-th ts.ttl
-  mkdir -p ../reports
-  mv $(echo earl*.ttl) ../reports
-)
+if [ ! $NoReport -eq 0 ]
+then
+  echo "Generating EARL reports"
+  (
+    cd "test harness"
+    ./rdb2rdf-th ts.ttl
+    mkdir -p ../reports
+    mv $(echo earl*.ttl) ../reports
+  )
+fi
